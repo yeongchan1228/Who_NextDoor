c4d7de97124ba9e0b1ec5ac2ded43ad44c3173cd
Jiwon <ljwon77@daum.net>
2021-04-14 16:51:42 +0900

    비밀번호 재설정

--- app/src/main/java/com/example/who_nextdoor/GofindpwdActivity.java
+++ app/src/main/java/com/example/who_nextdoor/GofindpwdActivity.java
@@ -7,15 +7,45 @@ import android.os.Bundle;
 import android.view.View;
 
 public class GofindpwdActivity extends AppCompatActivity {
+    private EditText email;
+    private Button requestEmail;
+    private ProgressDialog progressDialog;
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gofindpwd);
+        email = (EditText) findViewById(R.id.email);
+        requestEmail = (Button) findViewById(R.id.requestEmail);
+        progressDialog = new ProgressDialog(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        requestEmail.setOnClickListener(this);
+
     }
-    public void GoShowpasswd(View v){
-        Intent intent = new Intent(this, ShowpasswdActivity.class);
-        startActivity(intent);
-        finish();
+
+    public void onClick() {
+        progressDialog.setMessage("처리 중입니다. 잠시 기다려 주세요...");
+        progressDialog.show();
+
+        //비밀번호 재설정 이메일 보내기
+        String emailAddress = email.getText().toString().trim();
+        firebaseAuth.sendPasswordResetEmail(emailAddress)
+                    .addOnCompleteListener(new onCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(FindActivity.this, "이메일을 보냈습니다.", Toast.LENGTH_LONG).show();
+                                finish();
+                                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                            } else {
+                                Toast.makeText(FindActivity.this, "메일 보내기 실패!", Toast.LENGTH_LONG).show();
+                            }
+                            progressDialog.dismiss();
+                        }
+                    });
     }
 }
+
