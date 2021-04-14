afe58a4000d8e2066105f80f8068e91eab4f4bbd
asdbn <asdbnm066@daum.net>
2021-04-14 16:39:06 +0900

    GoJoin2

--- app/src/main/java/com/example/who_nextdoor/MainActivity.java
+++ app/src/main/java/com/example/who_nextdoor/MainActivity.java
@@ -65,6 +65,12 @@ public class MainActivity extends AppCompatActivity{
         finish();
     }
 
+    public void GoJoin2(View v){
+        Intent intent = new Intent(this, com.example.who_nextdoor.Join2.class);
+        startActivity(intent);
+        finish();
+    }
+
     public void GoStart(View v){
         Intent intent = new Intent(this, com.example.who_nextdoor.StartActivity.class);
         startActivity(intent);
