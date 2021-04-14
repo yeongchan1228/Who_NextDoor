2550989452bfe6354c0fb0725f582314fd83e059
gks54 <gks54410@gmail.com>
2021-04-14 17:01:13 +0900

    수정

--- app/src/main/java/com/example/who_nextdoor/GofindpwdActivity.java
+++ app/src/main/java/com/example/who_nextdoor/GofindpwdActivity.java
@@ -1,10 +1,20 @@
 package com.example.who_nextdoor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class GofindpwdActivity extends AppCompatActivity {
     private EditText email;
@@ -22,8 +32,6 @@ public class GofindpwdActivity extends AppCompatActivity {
         progressDialog = new ProgressDialog(this);
         firebaseAuth = FirebaseAuth.getInstance();
 
-        requestEmail.setOnClickListener(this);
-
     }
 
     public void onClick() {
@@ -33,19 +41,20 @@ public class GofindpwdActivity extends AppCompatActivity {
         //비밀번호 재설정 이메일 보내기
         String emailAddress = email.getText().toString().trim();
         firebaseAuth.sendPasswordResetEmail(emailAddress)
-                    .addOnCompleteListener(new onCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if(task.isSuccessful()){
-                                Toast.makeText(FindActivity.this, "이메일을 보냈습니다.", Toast.LENGTH_LONG).show();
-                                finish();
-                                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-                            } else {
-                                Toast.makeText(FindActivity.this, "메일 보내기 실패!", Toast.LENGTH_LONG).show();
-                            }
-                            progressDialog.dismiss();
-                        }
-                    });
+        .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(GofindpwdActivity.this, "이메일을 보냈습니다.", Toast.LENGTH_LONG).show();
+                    finish();
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                } else {
+                    Toast.makeText(GofindpwdActivity.this, "메일 보내기 실패!", Toast.LENGTH_LONG).show();
+                }
+                progressDialog.dismiss();
+            }
+        });
+        }
     }
-}
+
 
