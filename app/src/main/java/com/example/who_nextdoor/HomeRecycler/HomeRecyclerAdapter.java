9e55607734447b424a600fd322777d581fbdec8c
gks54 <gks54410@gmail.com>
2021-05-13 23:53:50 +0900

    Home recyclerview 생성, edittext 엔터 처리

---
+++ app/src/main/java/com/example/who_nextdoor/HomeRecycler/HomeRecyclerAdapter.java
@@ -0,0 +1,60 @@
+package com.example.who_nextdoor.HomeRecycler;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.who_nextdoor.R;
+
+import java.util.ArrayList;
+
+public class HomeRecyclerAdapter extends RecyclerView.Adapter<HomeRecyclerAdapter.ItemViewHolder> {
+    private ArrayList<Data> listData = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        holder.onBind(listData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
+    public void addItem(Data data) { // item 추가
+        listData.add(data);
+    }
+
+    class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textView1;
+        private TextView textView2;
+        private ImageView imageView;
+
+        ItemViewHolder(View itemView) {
+            super(itemView);
+
+            textView1 = itemView.findViewById(R.id.textView1);
+            textView2 = itemView.findViewById(R.id.textView2);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+
+        void onBind(Data data) {
+            textView1.setText(data.getTitle());
+            textView2.setText(data.getContent());
+            imageView.setImageResource(data.getResId());
+        }
+    }
+}
