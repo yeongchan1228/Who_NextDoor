d43843df32b6bb98c865fd64b8d3519cf1b86e4a
Jiwon <ljwon77@daum.net>
2021-06-13 19:58:09 +0900

    etc

--- app/src/main/java/com/example/who_nextdoor/Activity/ChatActivity.java
+++ app/src/main/java/com/example/who_nextdoor/Activity/ChatActivity.java
@@ -32,9 +32,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import android.text.TextUtils;
-import android.util.Log;
-import com.example.who_nextdoor.UserInfo;
 
 public class ChatActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -51,7 +48,6 @@ public class ChatActivity extends AppCompatActivity {
 
     private String nick = "익명";
     private String id = user.getUid();
-    private String nickname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +63,52 @@ public class ChatActivity extends AppCompatActivity {
 
         Button_send = findViewById(R.id.Button_send);
         EditText_chat = findViewById(R.id.EditText_chat);
+
+
+        Button_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = EditText_chat.getText().toString();
+
+                if(msg != null) {
+                    ChatDataInfo chat = new ChatDataInfo();
+
+                    chat.setUid(user.getUid());
+                    chat.setNickname(nick);
+                    chat.setMsg(msg);
+                    chat.setDate(getTime());
+                    collectionReference.add(chat);
+                    recyclerView = findViewById(R.id.my_recyclerView);
+                    recyclerView.setHasFixedSize(true); // 성능 강화
+                    layoutManager = new LinearLayoutManager(ChatActivity.this);
+                    recyclerView.setLayoutManager(layoutManager);
+
+                    chatList = new ArrayList<>();
+                    adapter = new ChatAdapter(chatList, ChatActivity.this, nick, id);
+                    recyclerView.setAdapter(adapter);
+
+                    collectionReference.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                        @Override
+                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                            chatList.clear();
+                            for(DocumentSnapshot documentSnapshot : queryDocumentSnapshots.getDocuments()){
+                                ChatDataInfo chatDataInfo = documentSnapshot.toObject(ChatDataInfo.class);
+                                chatList.add(chatDataInfo);
+                            }
+                            Collections.sort(chatList);
+                            adapter.notifyDataSetChanged();
+                            EditText_chat.setText(null);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(ChatActivity.this,"채팅 내용이 없습니다.",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
 
         recyclerView = findViewById(R.id.my_recyclerView);
         recyclerView.setHasFixedSize(true); // 성능 강화
@@ -105,73 +147,5 @@ public class ChatActivity extends AppCompatActivity {
         mDate = new Date(mNow);
         return mFormat.format(mDate);
     }
-
-    public void Chat_Button(View v){
-        String msg = EditText_chat.getText().toString();
-
-        if(!TextUtils.isEmpty(msg)) {
-            ChatDataInfo chat = new ChatDataInfo();
-            FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
-            CollectionReference collectionReference2 = firebaseFirestore.collection("users");
-
-
-            collectionReference2.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                @Override
-                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                    for(DocumentSnapshot documentSnapshot : queryDocumentSnapshots.getDocuments()){
-                        UserInfo userInfo = documentSnapshot.toObject(UserInfo.class);
-                        if(user.getEmail().equals(userInfo.getAddress())){
-                            nickname = userInfo.getShcoolNumber();
-                            StringBuilder builder = new StringBuilder(nickname);
-                            builder.setCharAt(2,'*');
-                            builder.setCharAt(3,'*');
-                            builder.setCharAt(6,'*');
-                            builder.setCharAt(7,'*');
-                            chat.setNickname(builder.toString());
-                            chat.setUid(user.getUid());
-                            chat.setMsg(msg);
-                            chat.setDate(getTime());
-                            collectionReference.add(chat);
-                            recyclerView = findViewById(R.id.my_recyclerView);
-                            recyclerView.setHasFixedSize(true); // 성능 강화
-                            layoutManager = new LinearLayoutManager(ChatActivity.this);
-                            recyclerView.setLayoutManager(layoutManager);
-
-                            chatList = new ArrayList<>();
-                            adapter = new ChatAdapter(chatList, ChatActivity.this, nick, id);
-                            recyclerView.setAdapter(adapter);
-
-                            collectionReference.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                                @Override
-                                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                                    chatList.clear();
-                                    for(DocumentSnapshot documentSnapshot : queryDocumentSnapshots.getDocuments()){
-                                        ChatDataInfo chatDataInfo = documentSnapshot.toObject(ChatDataInfo.class);
-                                        chatList.add(chatDataInfo);
-                                    }
-                                    Collections.sort(chatList);
-                                    adapter.notifyDataSetChanged();
-                                    EditText_chat.setText(null);
-                                }
-                            }).addOnFailureListener(new OnFailureListener() {
-
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Toast.makeText(ChatActivity.this,"채팅 내용이 없습니다.",Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                            break;
-                        }
-                    }
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                }
-            });
-
-        }
-    }
-
 
 }
